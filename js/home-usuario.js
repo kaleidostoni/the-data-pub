096b8018baf92ee770a3064b4784e8419f3621a2
Aciremaaleman <aciremaaleman@outlook.es>
2018-04-24 11:25:53 -0500

    Adding home user

--- js/home-usuario.js
+++ js/home-usuario.js
@@ -1,7 +1,2 @@
-const array = [];
 
-window.onload = function(toLocalStorage) {
-    const getInfoGoogle = localStorage.getItem('usuario', JSON.parse(toLocalStorage));
-    console.log(getInfoGoogle);
 
-}
--- js/perfil-usuario.js
+++ js/perfil-usuario.js
@@ -2,21 +2,24 @@ const btnNext = () => {
     window.location.href = "../views/perfil2-user.html";
 }
 
-var inputOne = document.getElementById('study');
-var inputTwo = document.getElementById('company');
-var inputThree = document.getElementById('experience');
-var subBtn = document.getElementById('formUserSubmit');
+const inputOne = document.getElementById('study');
+const inputTwo = document.getElementById('company');
+const inputThree = document.getElementById('experience');
+const divProfileForm = document.getElementById('profileDataForm');
+const subBtn = document.getElementById('formUserSubmit');
 
 subBtn.addEventListener('click', function createVacant(e) {
     e.preventDefault();
     const valOne = inputOne.value;
     const valTwo = inputTwo.value;
     const valThree = inputThree.value;
-    createObject(valOne, valTwo, valThree);
+
+    createObject(valOne, valTwo, valTwo);
+    console.log(createObject(valOne, valTwo, valTwo));
 });
 
 const createObject = (valOne, valTwo, valThree) => {
-    //console.log(valOne, valTwo, valThree);
+    console.log(valOne, valTwo, valThree);
     let object = [{
         firstInput: valOne,
         secondInput: valTwo,
@@ -27,8 +30,42 @@ const createObject = (valOne, valTwo, valThree) => {
 
 const objectToLocalStorage = object => {
     const toLocalStorage = localStorage.setItem('profileUserStorage', JSON.stringify(object));
-    window.location.href = "../views/home-user.html"
+    console.log(toLocalStorage);
+    const getDataLocalStorage = localStorage.getItem('profileUserStorage')
+    console.log(getDataLocalStorage);
+    const parseData = JSON.parse(getDataLocalStorage);
+    console.log(parseData);
+    //console.log(toLocalStorage,getDataLocalStorage)
+    gettingData(parseData);
 }
+
+const gettingData = (parseData) => {
+
+    parseData.forEach(element => {
+        const elementOne = element.firstInput;
+        console.log(elementOne);
+        const elementTwo = element.SecondInput;
+        console.log(elementTwo);
+        const elementThree = element.ThreeInput;
+    
+        paintData(elementOne,elementTwo,elementThree)
+    });
+
+const paintData = (elementOne,elementTwo,elementThree) => {
+
+    let template = `<div>
+                    <p>${elementOne}</p>
+                    <p>${elementTwo}</p>
+                    <p>${elementThree}</p>
+                    </div>`
+    
+    divProfileForm.innerHTML = template;
+    divProfileForm.append(template);
+}
+
+window.location.href = "../views/home-user.html"
+}
+
 
 
 
