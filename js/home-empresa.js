baef960d02affaf13f0db16216247e8470e13527
kaleidostoni <kaleidostoni@gmail.com>
2018-04-24 12:38:35 -0500

    pintando vacante

--- js/home-empresa.js
+++ js/home-empresa.js
@@ -1,5 +1,8 @@
 //CUESTIONARIO DE VACANTES
 
+//contenedor de vacantes 
+const jobVacantsSection = $('#job-vacants-section');
+
 // Esta función guarda el valor de la primer pregunta(select)
 var question1 = $( "#question1" ) ;
 const savingSelectedOption = ()=>{
@@ -9,7 +12,6 @@ const savingSelectedOption = ()=>{
     });
 }
 question1.change(savingSelectedOption);
-
 
 // Esta función guarda los valores del resto de las preguntas
 submitBtn = $ ( "#submit-btn" )
@@ -21,8 +23,33 @@ const savingAnswers = (e)=>{
     const question5 = $( "#question5").val();
     const question6 = $( "#question6").val();
     const question7 = $( "#question7").val();
-    const question8 = $( "#question8").val();
-paintingJobVacant()
+paintingJobVacant(question2,question3,question4,question5,question6,question7);
 };
 
 submitBtn.click(savingAnswers);
+//funcion que pinta la vacante 
+const paintingJobVacant = (q2, q3, q4, q5, q6, q7) => {
+   let template
+    template += `<div class="card">
+    <div class="card-header">
+    </div>
+    <div class="card-body">
+      <h5 class="card-title">TITLE</h5>
+      <h4>Capacidades<h4>
+      <p class="card-text">${q2}</p>
+      <h4>Día a día<h4>
+      <p class="card-text">${q3}</p>
+      <h4>Valor<h4>
+      <p class="card-text">${q4}</p>
+      <h4>Sueldo y ezquema de pago<h4>
+      <p class="card-text">${q5}</p>
+      <h4>Prestaciones<h4>
+      <p class="card-text">${q6}</p>
+      <h6>Capacidades<h6>
+      <p class="card-text">${q7}</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>`
+
+  jobVacantsSection.html(template);
+ }
