dbca1dd675fd05f82f906c3711bd23b009677213
Aciremaaleman <aciremaaleman@outlook.es>
2018-04-24 15:49:01 -0500

    Add home user

--- js/home-usuario.js
+++ js/home-usuario.js
@@ -1,17 +1,24 @@
 window.onload = function() {
-    const getDataLocalStorage = localStorage.getItem('profileUserStorage')
+    const getDataLocalStorage = localStorage.getItem('profileUserStorage');
     const parsedData = JSON.parse(getDataLocalStorage);
     //console.log(parseData);
 
-    const gettingData = (parsedData) => {
+    //const getPhotoGoogle = localStorage.getItem('usuario');
+    //const parseDataGoogle = JSON.parse(getPhotoGoogle);
+    //console.log(parseDataGoogle);
+
+    const gettingData = (parsedData,parsedDataGoogle) => {
         let divProfileForm = document.getElementById('profileDataForm');
         let output;
         let nodeList = Array.from(document.querySelectorAll('.form-control'))
-        let userInfo = parsedData[0]
+        let userInfo = parsedData[0];
+        console.log(userInfo);
+        //let userGoogle = parsedDataGoogle;
 
         let html = `<div class="card">
             <div class="card-header">
-                Info del usuario
+            <img id="photo" src="">
+            <p id="name"></p>
             </div>
             <div class="card-body">
                 <h5 class="card-title">${userInfo.firstInput}</h5>
